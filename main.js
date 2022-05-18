41b94b7ca21bcd44f52cd3034635fa7096196881
shaurya2405 <92263901+shaurya2405@users.noreply.github.com>
2022-05-18 20:50:49 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+function startClassification()
+{
+navigator.mediaDevices.getUserMedia({audio: true}); 
+classifier = ml5.soundClassifier('https://teachablemachine.withgoogle.com/models/wzLioEwvQ/', modelReady);
+}
+
+function modelReady()
+{
+classifier.classify(gotResults);    
+}
